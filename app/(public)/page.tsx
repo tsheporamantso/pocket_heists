918a4b044aaaf44490b4b83990eb7aecbe53601d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-19 10:12:42 +0200

    style: add basic styles on paragraph

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,22 +2,23 @@
 // when logged in --> to /heists
 // when not logged in --> to /login
 
-import { Clock8 } from "lucide-react"
+import { Clock8 } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="center-content">
       <div className="page-content">
         <h1>
-          P<Clock8 className="logo" strokeWidth={2.75} />cket Heist
+          P<Clock8 className="logo" strokeWidth={2.75} />
+          cket Heist
         </h1>
         <div>Tiny missions. Big office mischief.</div>
-        <p>
-          Welcome to Pocket Heist — the game where you plan and execute
-          tiny office missions for maximum fun. Assemble your crew, pick your
-          target, and pull off the perfect heist before anyone notices.
+        <p className="mt-8 max-w[480px] leading-5">
+          Welcome to Pocket Heist — the game where you plan and execute tiny
+          office missions for maximum fun. Assemble your crew, pick your target,
+          and pull off the perfect heist before anyone notices.
         </p>
       </div>
     </div>
-  )
+  );
 }
